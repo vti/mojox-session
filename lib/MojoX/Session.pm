cdab1572336814ff9447295d30eaeab20e491f62
vti <viacheslav.t@gmail.com>
2009-08-16 12:39:48 +0200

    Updated credits and fixed my name

--- lib/MojoX/Session.pm
+++ lib/MojoX/Session.pm
@@ -393,11 +393,13 @@ vti, C<vti@cpan.org>.
 
 =head1 CREDITS
 
+David Davis
+
 William Ono
 
 =head1 COPYRIGHT
 
-Copyright (C) 2008, Viacheslav Tikhanovskii.
+Copyright (C) 2008, Viacheslav Tykhanovskyi.
 
 This program is free software, you can redistribute it and/or modify it under
 the same terms as Perl 5.10.
